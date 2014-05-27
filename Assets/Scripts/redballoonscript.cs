933d2f9ce650da0be6449abc485b9a8b29817c68
Chielo Zimmerman <chielo@coshx.com>
2014-05-27 15:47:45 -0400

    makes balls start to deflate only upon contact with square

--- Assets/Scripts/redballoonscript.cs
+++ Assets/Scripts/redballoonscript.cs
@@ -2,59 +2,39 @@
 using System.Collections;
 
 public class redballoonscript : MonoBehaviour {
-    //speed and movement direction
-    public Vector2 speed = new Vector2(0, 2);
-
-    public Vector2 direction = new Vector2(0, 1);
-
-    public Vector2 movement;
-
-
-    //deflationrate
-    public float deflaterate = .002f;
+    
+	public Vector2 speed = new Vector2(0, 2);
+	public Vector2 direction = new Vector2(0, 1);
+	public Vector2 movement;
+	private float deflaterate = .0f;
     public float accel = 3;
+	public int floatingconst = 4;
 
-    public int floatingconst = 4;
-	// Use this for initialization
-	void Start () {
+	void Start () {}
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-        movement = new Vector2(
-            speed.x * direction.x,
-            speed.y * direction.y / floatingconst);
-        deflate();
-        speed += new Vector2(
-            0,
-            .01f);
-
+        movement = new Vector2( speed.x * direction.x, speed.y * direction.y / floatingconst);
+        this.deflate();
+        speed += new Vector2( 0, .01f);
         this.transform.Translate(Vector3.up * accel * Time.deltaTime);
+
 	}
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         rigidbody2D.velocity = movement;
     }
 
-    void reset()
-    {
+    void reset() {
         Vector3 originalscale = new Vector3(1, 1, 1);
         transform.localScale = originalscale;
-
     }
 
-    void deflate()
-    {
+    void deflate() {
         float xcheck = transform.localScale.x;
-        if (xcheck >= 0)
-        {
+        if (xcheck >= 0) {
             Vector3 reduce = new Vector3(deflaterate, deflaterate, 0);
             transform.localScale -= reduce;
-        }
-        else
-        {
+        } else {
             Destroy(gameObject);
         }
 
@@ -65,6 +45,8 @@ public class redballoonscript : MonoBehaviour {
     }
 
     public void setDeflateRate(float newrate){
-        deflaterate = newrate;
+
+		deflaterate = newrate;
+
     }
 }
