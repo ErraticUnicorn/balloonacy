48b42ec10e2d0b74bc58752c61a892a11e116cfa
Chielo Zimmerman <chielo@coshx.com>
2014-05-27 14:39:55 -0400

    fixes trival whitespace issues

--- Assets/Scripts/playerscript.cs
+++ Assets/Scripts/playerscript.cs
@@ -2,126 +2,85 @@
 using System.Collections;
 
 public class playerscript : MonoBehaviour {
-
-
-    public Vector2 movement;
-    //controls movementspeed
+	public Vector2 movement;
     public float movementSpeed = 5;
-
-    //jump controls
-    private int jumpHeight = 500;
+	public Vector3 startpos = new Vector3(0, 0, 0);
+	private int jumpHeight = 500;
     private bool isGrounded = false;
+	private bool losing = false;
+	private GameObject CameraCheck;
 
-    //starting position
-    public Vector3 startpos = new Vector3(0, 0, 0);
-
-    //losing
-    private bool losing = false;
-
-    private GameObject CameraCheck;
-
-	// Use this for initialization
 	void Start () {
         CameraCheck = GameObject.Find("Main Camera");
 	}
 	
-	// Update is called once per frame 
 	void Update () {
-        //section deals with input
-        //Keyboard input helper function
-        KeyboardControls();
-
-        //MouseControls
-        //MouseControls();
-
-        //iOS
-        iOSControls();
-
-        //Bounday Checks
-        CheckBounds();
-      
-        //checks if lost
-        Lose();
+       	this.keyboardControls();
+		this.iOSControls();
+        this.checkBounds();
+        this.checkForDidLose();
 	}
 
-    void FixedUpdate()
-    {
-        
-    }
-    //handles keyboard input
-    void KeyboardControls()
-    {
-        //allows left and right movement only when jumping
-        if (Input.GetKey("left") && !isGrounded)
-        {
-            XAxisMvmtLeft();
+    void FixedUpdate() {}
+    
+	void keyboardControls(){
+        if (Input.GetKey("left") && !isGrounded){
+            xAxisMvmtLeft();
         }
 
-        if (Input.GetKey("right") && !isGrounded)
-        {
-            XAxisMvmtRight();
+        if (Input.GetKey("right") && !isGrounded){
+            xAxisMvmtRight();
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            Jump();
+        if (Input.GetButtonDown("Jump") && isGrounded){
+            playerJump();
         }
 
     }
 
-    void MouseControls()
-    {
-        if (Input.GetMouseButtonDown(0))//&& !isGrounded)
-        {
-            Jump();
+    void mouseControls() {
+
+        if (Input.GetMouseButtonDown(0)) {
+            playerJump();
         }
 
-        if (Input.GetAxis("Mouse X") <= -1)
-        {
-            XAxisMvmtLeft();
+        if (Input.GetAxis("Mouse X") <= -1) {
+            xAxisMvmtLeft();
         }
 
-        //ray for the purpose of converting a click on screen to points in the game world
-        var coordRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (coordRay.origin.x < transform.position.x && !isGrounded)
-        {
-            XAxisMvmtLeft();
-
+        var screenPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+  		
+		if (screenPoint.origin.x < transform.position.x && !isGrounded) {
+            xAxisMvmtLeft();
         }
 
-        if (coordRay.origin.x > transform.position.x && !isGrounded)
-        {
-            XAxisMvmtRight();
+		if (screenPoint.origin.x > transform.position.x && !isGrounded) {
+            xAxisMvmtRight();
         }
     }
 
-    void iOSControls()
-    {
-        foreach (Touch touch in Input.touches){
-            if (touch.phase == TouchPhase.Began)
-            {
-                Jump();
+	void iOSControls() {
+
+        foreach (Touch touch in Input.touches) {
+            
+			if (touch.phase == TouchPhase.Began) {
+                playerJump();
             }
 
-            //ray for the purpose of converting a touch on screen to points in the game world
-            var coordRay = Camera.main.ScreenPointToRay(touch.position);
-            if (coordRay.origin.x < transform.position.x && !isGrounded)
-            {
-                XAxisMvmtLeft();
-
+			var screenPoint = Camera.main.ScreenPointToRay(touch.position);
+			if (screenPoint.origin.x < transform.position.x && !isGrounded) {
+                xAxisMvmtLeft();
             }
 
-            if (coordRay.origin.x > transform.position.x && !isGrounded)
-            {
-                XAxisMvmtRight();
+			if (screenPoint.origin.x > transform.position.x && !isGrounded) {
+                xAxisMvmtRight();
             }
         }
 
         Vector3 dir = Vector3.zero;
 
-        dir.x = -Input.acceleration.y;
-        dir.y = Input.acceleration.x;
+        dir.x = Input.acceleration.x;
+        //dir.y = -Input.acceleration.y;
 
         if (dir.sqrMagnitude > 1)
         {
@@ -132,39 +91,27 @@ public class playerscript : MonoBehaviour {
         transform.position += dir * movementSpeed;
     }
 
-    void Jump()
-    {
-        if (!isGrounded)
-        {
+    void playerJump() {
+        if (!isGrounded) {
             return;
         }
         isGrounded = false;
         rigidbody2D.AddForce(new Vector2(0, jumpHeight));
-
     }
 
-    void XAxisMvmtRight()
-    {
+    void xAxisMvmtRight() {
         transform.position += Vector3.right * movementSpeed * Time.deltaTime;
 
     }
-    void XAxisMvmtLeft()
-    {
+    void xAxisMvmtLeft() {
         transform.position -= Vector3.right * movementSpeed * Time.deltaTime;
     }
-
-
-    void CheckBounds()
-    {
-        //make sure the player doesn't leave the upper y, and two x values
+	
+	void checkBounds() {
         var dist = (transform.position - Camera.main.transform.position).z;
-
-        var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
-
-        var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
-
-        //needs work
-        var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 2, dist)).y;
+		var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+		var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+	    var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 2, dist)).y;
 
         transform.position = new Vector3(
             Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
@@ -173,43 +120,45 @@ public class playerscript : MonoBehaviour {
 
             );
     }
-    //lose state
-    void Lose()
-    {
-        //lose state
-        if (checkIfOutOfBounds())
-        {
+    
+	void checkForDidLose() {
+		if (checkIfOutOfBounds()){
             losing = true;
         }
 
-        if (losing)
-        {
+        if (losing) {
             Application.LoadLevel("losescreen");
         }
 
     }
 
-
-    //checks to see if player has landed on balloon as well as triggering events for balloons
-    void OnCollisionEnter2D(Collision2D info)
-    {
-        if (rigidbody2D.velocity.magnitude <= 3  )
-        {
+	void OnCollisionEnter2D(Collision2D info) {
+        if (rigidbody2D.velocity.magnitude <= 3  ) {
             isGrounded = true;
         }
         //increase deflaterate of balloon once player steps on it
-        if (info.gameObject.tag == "redplatform")
-        {
-            
+        if (info.gameObject.tag == "redplatform") {
             var curballoon = info.gameObject.GetComponent<redballoonscript>();
             curballoon.setDeflateRate(.006f);
         }
-
-        
     }
 
-    // Maybe allow balloons to deflate slower once you jump off?
-    /*
+    
+    bool checkIfOutOfBounds() {
+        if (transform.position.y <= CameraCheck.transform.position.y - 10 || transform.position.x >= 10 || transform.position.x <= -10)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+
+// Maybe allow balloons to deflate slower once you jump off?
+/*
     void OnCollisionExit2D(Collision2D info)
     {
         if (info.gameObject.tag == "redplatform")
@@ -222,21 +171,9 @@ public class playerscript : MonoBehaviour {
     }
     */
 
-    //checks if player has fallen out of bounds.
-    bool checkIfOutOfBounds()
-    {
-        if (transform.position.y <= CameraCheck.transform.position.y - 10 || transform.position.x >= 10 || transform.position.x <= -10)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    //gives functionality to 2D rigidbodies to have different types of forces
-    Vector2 ApplyForceMode(Vector2 force, ForceMode forceMode)
+//gives functionality to 2D rigidbodies to have different types of forces
+/*
+	Vector2 ApplyForceMode(Vector2 force, ForceMode forceMode)
     {
         switch (forceMode)
         {
@@ -253,5 +190,5 @@ public class playerscript : MonoBehaviour {
 
         return force;
     }
-}
+    */
 
