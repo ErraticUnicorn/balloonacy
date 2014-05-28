6f2b26dbfd53f05d3af3baf1488da28b3669f4a4
David Wert <djw2yw@virginia.edu>
2014-05-28 13:10:20 -0400

    Adds title screen
    And organization tweaks.

---
+++ Assets/Scripts/Buttons/startbutton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startbutton : MonoBehaviour {
+
+    void OnGUI()
+    {
+        Debug.Log("Hi!");
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 100, 50), "Click to Begin!")) //for adding a texture just replace the string witht he image
+        {
+            Application.LoadLevel("stage1");
+        }
+    }
+}
