269b99e00cf7942a0656caf2aa0748efbcba4c8d
David Wert <djw2yw@virginia.edu>
2014-05-30 13:00:08 -0400

    Adds Singular Balloon Class

---
+++ Assets/Scripts/Balloons/balloon.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+ public class balloon : MonoBehaviour {
+
+    public Vector2 speed;
+    public Vector2 direction;
+    public Vector2 movement;
+    public float deflaterate;
+    public float accel;
+    public int floatingconst;
+    public bool isVisible = false;
+    public bool hasBecomeVisible = false;
+
+    protected Vector3 originalScale;
+
+    void Start()
+    {
+        originalScale = this.transform.localScale;
+    }
+
+    protected void Update()
+    {
+        movement = new Vector2(speed.x * direction.x, speed.y * direction.y / floatingconst);
+        this.deflate();
+        speed += new Vector2(0, .01f);
+        this.transform.Translate(Vector3.up * accel * Time.deltaTime);
+
+        if (!isVisible && transform.localScale.x < .5 && !hasBecomeVisible)
+        {
+            Invoke("Destroy", 1f);
+        }
+    }
+
+    protected void FixedUpdate()
+    {
+        rigidbody2D.velocity = movement;
+    }
+
+    protected void Destroy()
+    {
+        this.gameObject.SetActive(false);
+        resetScale();
+    }
+
+    protected void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    public void OnBecameVisible()
+    {
+        isVisible = true;
+        hasBecomeVisible = true;
+    }
+
+    public void OnBecameInvisible()
+    {
+        Invoke("Destroy", 0f);
+        isVisible = false;
+    }
+
+    public void resetScale()
+    {
+        this.transform.localScale = originalScale;
+    }
+
+    protected void reset()
+    {
+        Vector3 originalscale = new Vector3(1, 1, 1);
+        transform.localScale = originalscale;
+    }
+
+    protected void deflate()
+    {
+        float xcheck = transform.localScale.x;
+        if (xcheck >= 0)
+        {
+            Vector3 reduce = new Vector3(deflaterate, deflaterate, 0);
+            transform.localScale -= reduce;
+        }
+        else
+        {
+            Invoke("Destroy", 1f);
+        }
+
+    }
+
+    public float getDeflateRate()
+    {
+        return deflaterate;
+    }
+
+    public void setDeflateRate(float newrate)
+    {
+        deflaterate = newrate;
+    }
+}
--- Assets/Scripts/Balloons/redballoonscript.cs
+++ Assets/Scripts/Balloons/redballoonscript.cs
@@ -1,89 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class redballoonscript : MonoBehaviour {
-    
-	public Vector2 speed = new Vector2(0, 2.5f);
-	public Vector2 direction = new Vector2(0, 1);
-	public Vector2 movement;
-	public float deflaterate = .001f;
-    public float accel = 4.5f;
-	public int floatingconst = 4;
-    public bool isVisible = false;
-    public bool hasBecomeVisible = false;
-
-    private Vector3 originalScale;
+ public class redballoonscript : balloon {
 
     void Start()
     {
-        originalScale = this.transform.localScale;
-    }
-	
-	void Update () {
-        movement = new Vector2( speed.x * direction.x, speed.y * direction.y / floatingconst);
-        this.deflate();
-        speed += new Vector2( 0, .01f);
-        this.transform.Translate(Vector3.up * accel * Time.deltaTime);
-
-        if (!isVisible && transform.localScale.x < .5 && !hasBecomeVisible)
-        {
-            Invoke("Destroy", 1f);
-        }
-	}
-
-    void FixedUpdate() {
-        rigidbody2D.velocity = movement;
-    }
-
-    void reset() {
-        Vector3 originalscale = new Vector3(1, 1, 1);
-        transform.localScale = originalscale;
-    }
-
-    void deflate() {
-        float xcheck = transform.localScale.x;
-        if (xcheck >= 0) {
-            Vector3 reduce = new Vector3(deflaterate, deflaterate, 0);
-            transform.localScale -= reduce;
-        } else {
-            Invoke("Destroy", 1f);
-        }
-
-    }
-
-    public float getDeflateRate() {
-        return deflaterate;
-    }
-
-    public void setDeflateRate(float newrate) {
-
-		deflaterate = newrate;
-    }
-
-    public void OnBecameVisible()
-    {
-        isVisible = true;
-        hasBecomeVisible = true;
-    }
-
-    public void OnBecameInvisible()
-    {
-        Invoke("Destroy", 0f);
-        isVisible = false;
-    }
-
-    public void resetScale(){
-        this.transform.localScale = originalScale;
-    }
-
-    void Destroy()
-    {
-        this.gameObject.SetActive(false);
-        resetScale();
-    }
-
-    void OnDisable()
-    {
-        CancelInvoke();
+        speed = new Vector2(0, 2.5f);
+        direction = new Vector2(0, 1);
+        deflaterate = .001f;
+        accel = 4.5f;
+        floatingconst = 4;
     }
 }
