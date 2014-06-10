7d07fbf3bc9e57e427cb807f172e15d7aa5cd74b
David Wert <djw2yw@virginia.edu>
2014-06-10 13:23:33 -0400

    Final White Space Issues

--- Assets/Model/BalloonModel.cs
+++ Assets/Model/BalloonModel.cs
@@ -2,10 +2,8 @@
 using System.Collections;
 
 
-namespace Models
-{
-    public class BalloonModel : MonoBehaviour
-    {
+namespace Models {
+    public class BalloonModel : MonoBehaviour {
 
         public Vector2 speed;
         public Vector2 direction;
@@ -27,56 +25,46 @@ namespace Models
 
         private BalloonController spawner;
 
-        public float getDeflateRate()
-        {
+        public float getDeflateRate() {
             return deflateRate;
         }
 
-        public Vector2 getSpeed()
-        {
+        public Vector2 getSpeed() {
             return speed;
         }
 
-        public float getAcceleration()
-        {
+        public float getAcceleration() {
             return accel;
         }
 
-        public Vector2 getDirection()
-        {
+        public Vector2 getDirection() {
             return direction;
         }
 
-        public void setSprite(Sprite image)
-        {
+        public void setSprite(Sprite image) {
             this.GetComponent<SpriteRenderer>().sprite = image;
         }
 
-        public void setDeflateRate(float newrate)
-        {
+        public void setDeflateRate(float newrate) {
             this.deflateRate = newrate;
         }
 
-        public void setSpeed(Vector2 newSpeed)
-        {
+        public void setSpeed(Vector2 newSpeed) {
             this.speed = newSpeed;
         }
 
-        public void setAcceleration(float accel)
-        {
+        public void setAcceleration(float accel) {
             Debug.Log(this.accel);
             this.accel = accel;
         }
 
-        public void setDirection(Vector2 newDirection)
-        {
+        public void setDirection(Vector2 newDirection) {
             this.direction = newDirection;
         }
 
 
         // Use this for initialization
-        void Start()
-        {
+        void Start() {
             floatingConst = 4;
             speedOffset = 0;
             originalScale = this.transform.localScale;
@@ -86,8 +74,7 @@ namespace Models
             this.GetComponent<SpriteRenderer>().sprite = curSprite;
         }
 
-        public void init(Vector2 speed, float deflaterate, float acceleration)
-        {
+        public void init(Vector2 speed, float deflaterate, float acceleration) {
             this.speed = speed;
             direction = new Vector2(0, 1);
             this.deflateRate = deflaterate;
@@ -96,8 +83,11 @@ namespace Models
         }
 
         // Update is called once per frame
-        protected void Update()
-        {
+        protected void Update() {
+            handleMovement();
+        }
+
+        protected void handleMovement() {
             movement = new Vector2(speed.x * direction.x, speed.y * direction.y / floatingConst);
             this.deflate();
             speed += new Vector2(0, .01f);
@@ -105,25 +95,21 @@ namespace Models
             var dist = 0;
             var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, dist)).y;
             var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
-            if (this.transform.localScale.y <= .2 && !soundPlayed)
-            {
+            if (this.transform.localScale.y <= .2 && !soundPlayed) {
                 audio.PlayOneShot(pop);
                 soundPlayed = true;
             }
-            if (this.transform.position.y > topBorder || (this.transform.position.y < bottomBorder - 20 && !hasBecomeVisible))
-            {
+            if (this.transform.position.y > topBorder || (this.transform.position.y < bottomBorder - 20 && !hasBecomeVisible)) {
                 Invoke("Destroy", .1f);
-            }           
+            } 
         }
 
-        protected void FixedUpdate()
-        {
+        protected void FixedUpdate() {
             rigidbody2D.velocity = movement;
 
         }
 
-        protected void Destroy()
-        {
+        protected void Destroy() {
             spawner.totalBalloons--;
             soundPlayed = false;
             this.transform.localScale = originalScale;
@@ -131,56 +117,44 @@ namespace Models
             this.gameObject.SetActive(false);
         }
 
-        protected void OnDisable()
-        {
+        protected void OnDisable() {
             CancelInvoke();
         }
 
-        public void OnBecameInvisible()
-        {
+        public void OnBecameInvisible() {
             //Invoke("Destroy", 0f);
 
             isVisible = false;
         }
 
-        public void OnBecameVisible()
-        {
+        public void OnBecameVisible() {
             isVisible = true;
         }
 
-        protected void deflate()
-        {
+        protected void deflate() {
             float xcheck = transform.localScale.x;
-            if (xcheck >= 0)
-            {
+            if (xcheck >= 0) {
                 Vector3 reduce = new Vector3(deflateRate, deflateRate, 0);
                 transform.localScale -= reduce;
             }
-            else
-            {
+            else {
                 Invoke("Destroy", 0f);
             }
-
         }
 
-        public void OnCollisionEnter2D(Collision2D other)
-        {
+        public void OnCollisionEnter2D(Collision2D other) {
             var contact = other.contacts[0];
-            if (contact.point.y < this.transform.position.y - .5f && other.gameObject.tag == "Player")
-            {
+            if (contact.point.y < this.transform.position.y - .5f && other.gameObject.tag == "Player") {
                 Invoke("Destroy", 0f);
             }
-            if (other.gameObject.tag == "platform" && !isVisible)
-            {
+            if (other.gameObject.tag == "platform" && !isVisible) {
                 Invoke("Destroy", 0f);
             }
             
         }
 
-        public void OnTriggerEnter2D(Collider2D other)
-        {
-            if (other.gameObject.tag == "platform" && !isVisible && !isGreen)
-            {
+        public void OnTriggerEnter2D(Collider2D other) {
+            if (other.gameObject.tag == "platform" && !isVisible && !isGreen) {
                 Invoke("Destroy", 0f);                
             }
         }
